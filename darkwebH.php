17786844ab38420cb414506898e8583380dc9e12
ScreamFR <59128084+ScreamFR@users.noreply.github.com>
2019-12-22 02:49:13 +0100

    Add files via upload

---
+++ darkwebH.php
@@ -0,0 +1,36 @@
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <title>DarkWed</title>
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../fontawesome/css/all.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+  </head>
+  
+  <body>
+      <img class="background" src="img/imgback5.jpg">
+      <img class="logo" src="img/logo.png">
+      <section>
+      <div class="Interface">
+          <p class="Title">DarkWeb</p>
+          </div>
+          <div class="MidInterface">
+          <div class="Mid_1">
+              <form method="get" action="">
+              <input class="TxtUsername" placeholder="Identifiants" type="text" required>
+              </div>
+              <div class="Mid_2">
+              <input class="TxtPassword" placeholder="clé d'activation" type="password" required>
+              </div>
+              <div class="BtV">
+              <input class="Bt" type="submit" value="Connexion" name="Bt">
+              </div>
+              </form>
+          </div>
+      </section>
+  </body>
+
+</html>
+
+
